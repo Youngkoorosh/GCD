3c6bb7ac3cb48d1f7b79ebd63c88f25265d23334
Youngkoorosh <koorosh.nrp@gmail.com>
2025-07-08 19:42:26 +0330

    change the variable name(GCD to gcd)
    to read better :
    I do this...

--- GCD_Project.py
+++ GCD_Project.py
@@ -1,18 +1,18 @@
 num1 = int(input("Enter num1: "))
 num2 = int(input("Enter num2: "))
 
-GCD = 0
+gcd = 0
 
 def GCD (num1, num2):
     while num1:
         if num1 % num2 == 0 :
-            GCD = num2
+            gcd = num2
             break
         else:
             num3 = num1 % num2
             num1 = num2
             num2 = num3
-    return GCD
+    return gcd
 
 print()
 
